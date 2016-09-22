a42fbe74bbbeae8cfeed1c13a5028b3f7ca424c6
Raimund Schlüßler <raimund.schluessler@biotec.tu-dresden.de>
2016-09-22 13:21:59 +0200

    Make calculation of background signal more robust

--- getBackground.m
+++ getBackground.m
@@ -2,10 +2,20 @@ function [ thres ] = getBackground( img )
 %GETBACKGROUND   most frequent value in a certain range of values
 %   backgroud = getBackground(img)
 
-    [N,edges] = histcounts(img,1000);
+    % chose number of bins so that the bin size is 0.5 pix
+    nrBins = round(max(img(:))/0.5);
     
+    % calculate the histogram
+    [N,edges] = histcounts(img,nrBins);
+    
+    % find the index of the most frequent value
     [~, ind] = max(N);
     
-    thres = mean(edges(ind:ind));
+    % calculate the threshold from the borders of the bin
+    thres = mean(edges(ind:ind+1));
+    
+    %% Check results
+%     figure(14);
+%     bar(edges(1:end-1),N)
     
 end
