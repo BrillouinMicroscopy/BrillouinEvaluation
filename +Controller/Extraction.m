41c52f1551c188e54044ad4163ab1671af464e9d
Raimund Schlüßler <raimund.schluessler@biotec.tu-dresden.de>
2017-01-20 12:53:52 +0100

    Clean view, copy code to controller, introduce option to remove peaks

--- +Controller/Extraction.m
+++ +Controller/Extraction.m
@@ -4,6 +4,7 @@ function extraction = Extraction(model, view)
     %% callbacks Calibration
     set(view.extraction.selectPeaks, 'Callback', {@selectPeaks, model});
     set(view.extraction.optimizePeaks, 'Callback', {@optimizePeaks, model});
+    set(view.extraction.clearPeaks, 'Callback', {@clearPeaks, model});
     
     set(view.extraction.extractionAxis, 'Callback', {@changeSettings, view, model});
     set(view.extraction.interpolationDirection, 'Callback', {@changeSettings, view, model});
@@ -35,6 +36,26 @@ function selectPeaks(~, ~, model)
         'x', x, ...
         'y', y ...
     );
+    fitSpectrum(model);
+end
+
+function clearPeaks(~, ~, model)
+    model.settings.extraction.peaks = struct( ...
+        'x', [], ...
+        'y', [] ...
+    );
+    model.settings.extraction.interpolationCenters = struct( ...
+        'x', [], ...        % [pix] x-position
+        'y', [] ...         % [pix] y-position
+    );
+    model.settings.extraction.interpolationBorders = struct( ...
+        'x', [], ...        % [pix] x-position
+        'y', [] ...         % [pix] y-position
+    );
+    model.settings.extraction.interpolationPositions = struct( ...
+        'x', [], ...        % [pix] x-position
+        'y', [] ...         % [pix] y-position
+    );
 end
 
 function optimizePeaks(~, ~, model)
@@ -63,6 +84,7 @@ function optimizePeaks(~, ~, model)
         end
         model.settings.extraction.peaks = peaks;
     end
+    fitSpectrum(model);
 end
 
 function changeSettings(~, ~, view, model)
@@ -80,6 +102,125 @@ function changeSettings(~, ~, view, model)
     extraction.width = str2double(get(view.extraction.width, 'String'));
     
     model.settings.extraction = extraction;
+    getInterpolationPositions(model);
+end
+
+function fitSpectrum(model)
+
+    newxb = model.settings.extraction.peaks.x;
+    newdata2b = model.settings.extraction.peaks.y;
+    circleStart = model.settings.extraction.circleStart;
+    
+    if ~sum(isnan(newxb)) && ~sum(isnan(newdata2b)) && ~sum(isnan(circleStart))
+
+        model2b = @(params) circleError(params, newxb, newdata2b, -1);
+        [estimates2b, ~, ~, ~] = fitCircle(model2b, newxb, circleStart);
+
+        model.settings.extraction.circleFit = estimates2b;
+    end
+    
+    getInterpolationPositions(model);
+
+    function [estimates2b, model2b, newxb, FittedCurve2b] = fitCircle(model2b, newxb, start)
+
+        options = optimset('MaxFunEvals', 100000, 'MaxIter', 100000);
+        estimates2b = fminsearch(model2b, start, options);
+
+        [~, FittedCurve2b] = model2b(estimates2b);
+    end
+    
+end
+
+function [error, y] = circleError(params, x, yTarget, sign)
+% CIRCLEERROR
+
+    y = circle(params, x, sign);
+
+    errorVec = y - yTarget;
+
+    error = sum(errorVec.^2);
+end
+
+function [y] = circle(params, x, sign)
+% CIRCLE model for a circle
+    y = params(2) + sign * sqrt(params(3).^2 - (x-params(1)).^2);
+    y(imag(y) ~=0) = NaN;
+end
+
+function getInterpolationPositions(model)
+
+%% calculate positions of the interpolation positions
+    img = model.file.readPayloadData(1, 1, 1, 'data');
+    params = model.settings.extraction.circleFit;
+    width = model.settings.extraction.width;
+    
+    centers.x = 1:size(img,2);
+    centers.y = 1:size(img,1);
+    switch model.settings.extraction.extractionAxis
+        case 'x'
+            centers.y = circle(params, centers.x, -1);
+        case 'y'
+            n(1) = params(2);
+            n(2) = params(1);
+            n(3) = params(3);
+            centers.x = circle(n, centers.y, 1);
+        case 'f'
+            centers.y = circle(params, centers.x, -1);
+            centers.y(~isreal(centers.y)) = NaN;
+            [yMax, ind] = max(centers.y);
+            xMax = centers.x(ind);
+            [yMin, ind] = min(centers.y);
+            xMin = centers.x(ind);
+            aMax = atan2((yMax - params(2)),(xMax - params(1)));
+            aMin = atan2((yMin - params(2)),(xMin - params(1)));
+            a = linspace(aMin, aMax, round(mean(size(img))));
+            centers.x = params(1) + params(3) * cos(a);
+            centers.y = params(2) + params(3) * sin(a);
+        otherwise
+            ex = MException('MATLAB:noSuchAxis', ...
+                'Not possible to use the axis %s. Chose either x, y or f.', p.Results.axis);
+            throw(ex)
+    end
+    
+    x0 = params(1);
+    y0 = params(2);
+    
+    m = (centers.y - y0) ./ (centers.x - x0);
+    alpha = atan(m);
+    
+    % preallocate borders arrays
+    borders = struct();
+    borders.x = NaN(2,length(centers.x));
+    borders.y = NaN(2,length(centers.x));
+    
+    switch model.settings.extraction.interpolationDirection
+        case 'f'
+            %% correct way to average the spectrum
+            borders.x = [1; 1] * centers.x + [-1; 1] .* width/2 * cos(alpha);
+            borders.y = [1; 1] * centers.y + [-1; 1] .* width/2 * sin(alpha);
+        case 'x'
+            %% "wrong" way to average the spectrum
+            % corresponds to the old way of averaging
+            borders.x = [1; 1] * centers.x + [-1; 1] .* width * sin(alpha);
+            borders.y = [1; 1] * centers.y;
+        case 'y'
+            %%
+            borders.x = [1; 1] * centers.x;
+            borders.y = [1; 1] * centers.y + [-1; 1] .* width * cos(alpha);
+        otherwise
+            ex = MException('MATLAB:noSuchAveraging', ...
+                'Not possible to average in direction %s. Chose either x, y or f.', p.Results.averaging);
+            throw(ex)
+    end
+
+    % create positions array for interpolating
+    steps = repmat(transpose(0:(width-1)),1,size(borders.y,2));
+    positions.x = repmat(borders.x(1,:),width,1) + repmat(diff(borders.x,1,1),width,1)./(width-1) .* steps;
+    positions.y = repmat(borders.y(1,:),width,1) + repmat(diff(borders.y,1,1),width,1)./(width-1) .* steps;
+    
+    model.settings.extraction.interpolationCenters = centers;
+    model.settings.extraction.interpolationBorders = borders;
+    model.settings.extraction.interpolationPositions = positions;
 end
 
 function setCameraParameters(UIControl, ~, model)
--- +View/Extraction.m
+++ +View/Extraction.m
@@ -15,12 +15,20 @@ end
 
 function handles = initGUI(model, parent)
 
-    selectPeaks = uicontrol('Parent', parent, 'Style','pushbutton', 'Units', 'normalized',...
-        'String','Select','Position',[0.02,0.92,0.1,0.055],...
+    manualPeaks = uicontrol('Parent', parent, 'Style','pushbutton', 'Units', 'normalized',...
+        'String','Manually','Position',[0.02,0.92,0.1,0.055],...
+        'FontSize', 11, 'HorizontalAlignment', 'left');
+    
+    autoPeaks = uicontrol('Parent', parent, 'Style','pushbutton', 'Units', 'normalized',...
+        'String','Automatically','Position',[0.14,0.92,0.1,0.055],...
         'FontSize', 11, 'HorizontalAlignment', 'left');
     
     optimizePeaks = uicontrol('Parent', parent, 'Style','pushbutton', 'Units', 'normalized',...
-        'String','Optimize','Position',[0.14,0.92,0.1,0.055],...
+        'String','Optimize','Position',[0.02,0.85,0.1,0.055],...
+        'FontSize', 11, 'HorizontalAlignment', 'left');
+    
+    clearPeaks = uicontrol('Parent', parent, 'Style','pushbutton', 'Units', 'normalized',...
+        'String','Clear','Position',[0.14,0.85,0.1,0.055],...
         'FontSize', 11, 'HorizontalAlignment', 'left');
     
     peakTable = uitable('Parent', parent, 'Units', 'normalized', 'Position', [0.02 0.5 0.22 0.3], ...
@@ -139,8 +147,10 @@ function handles = initGUI(model, parent)
         'plotCenters', plotCenters, ...
         'plotBorders', plotBorders, ...
         'zoomHandle', zoomHandle, ...
-        'selectPeaks', selectPeaks, ...
+        'selectPeaks', manualPeaks, ...
         'optimizePeaks', optimizePeaks, ...
+        'clearPeaks', clearPeaks, ...
+        'autoPeaks', autoPeaks, ...
         'peakTable', peakTable, ...
         'autoscale', autoscale, ...
         'cap', cap, ...
@@ -199,136 +209,30 @@ function onSettingsChange(handles, model)
     set(handles.extractionAxisGroup,'SelectedObject',findall(handles.extractionAxis, 'String', model.settings.extraction.extractionAxis));
     set(handles.interpolationDirectionGroup,'SelectedObject',findall(handles.interpolationDirection, 'String', model.settings.extraction.interpolationDirection));
     
-    tmp(:,1) = model.settings.extraction.peaks.x;
-    tmp(:,2) = model.settings.extraction.peaks.y;
-    if ~isnan(tmp)
-        handles.peakTable.Data = tmp;
-        handles.plotPeaks.XData = tmp(:,1);
-        handles.plotPeaks.YData = tmp(:,2);
-        fitSpectrum(model);
-        if isa(model.file, 'Utils.HDF5Storage.h5bm') && isvalid(model.file)
-            getInterpolationPositions(handles, model);
-        end
+    if ~sum(isnan(model.settings.extraction.peaks.x)) && ~sum(isnan(model.settings.extraction.peaks.y))
+        handles.peakTable.Data = transpose([model.settings.extraction.peaks.x; model.settings.extraction.peaks.y]);
+        handles.plotPeaks.XData = model.settings.extraction.peaks.x;
+        handles.plotPeaks.YData = model.settings.extraction.peaks.y;
+%         fitSpectrum(model);
+%         if isa(model.file, 'Utils.HDF5Storage.h5bm') && isvalid(model.file)
+%             getInterpolationPositions(handles, model);
+%         end
+    else
+        handles.peakTable.Data = [];
     end
+    showInterpolationPositions(handles, model);
 end
 
-function fitSpectrum(model)
-
-    newxb = model.settings.extraction.peaks.x;
-    newdata2b = model.settings.extraction.peaks.y;
-    circleStart = model.settings.extraction.circleStart;
-    
-    if ~sum(isnan(newxb)) && ~sum(isnan(newdata2b)) && ~sum(isnan(circleStart))
-
-        model2b = @(params) circleError(params, newxb, newdata2b, -1);
-        [estimates2b, ~, ~, ~] = fitCircle(model2b, newxb, circleStart);
-
-        model.settings.extraction.circleFit = estimates2b;
-    end
-
-    function [estimates2b, model2b, newxb, FittedCurve2b] = fitCircle(model2b, newxb, start)
-
-        options = optimset('MaxFunEvals', 100000, 'MaxIter', 100000);
-        estimates2b = fminsearch(model2b, start, options);
-
-        [~, FittedCurve2b] = model2b(estimates2b);
-    end
-    
-end
-
-function [error, y] = circleError(params, x, yTarget, sign)
-% CIRCLEERROR
-
-    y = circle(params, x, sign);
-
-    errorVec = y - yTarget;
-
-    error = sum(errorVec.^2);
-end
-
-function [y] = circle(params, x, sign)
-% CIRCLE model for a circle
-    y = params(2) + sign * sqrt(params(3).^2 - (x-params(1)).^2);
-    y(imag(y) ~=0) = NaN;
-end
-
-function getInterpolationPositions(handles, model)
-
-%% calculate positions of the interpolation positions
-    img = model.file.readPayloadData(1, 1, 1, 'data');
-    params = model.settings.extraction.circleFit;
-    width = model.settings.extraction.width;
-    
-    centers.x = 1:size(img,2);
-    centers.y = 1:size(img,1);
-    switch model.settings.extraction.extractionAxis
-        case 'x'
-            centers.y = circle(params, centers.x, -1);
-        case 'y'
-            n(1) = params(2);
-            n(2) = params(1);
-            n(3) = params(3);
-            centers.x = circle(n, centers.y, 1);
-        case 'f'
-            centers.y = circle(params, centers.x, -1);
-            centers.y(~isreal(centers.y)) = NaN;
-            [yMax, ind] = max(centers.y);
-            xMax = centers.x(ind);
-            [yMin, ind] = min(centers.y);
-            xMin = centers.x(ind);
-            aMax = atan2((yMax - params(2)),(xMax - params(1)));
-            aMin = atan2((yMin - params(2)),(xMin - params(1)));
-            a = linspace(aMin, aMax, round(mean(size(img))));
-            centers.x = params(1) + params(3) * cos(a);
-            centers.y = params(2) + params(3) * sin(a);
-        otherwise
-            ex = MException('MATLAB:noSuchAxis', ...
-                'Not possible to use the axis %s. Chose either x, y or f.', p.Results.axis);
-            throw(ex)
-    end
-    
-    x0 = params(1);
-    y0 = params(2);
-    
-    m = (centers.y - y0) ./ (centers.x - x0);
-    alpha = atan(m);
-    
-    % preallocate borders arrays
-    borders = struct();
-    borders.x = NaN(2,length(centers.x));
-    borders.y = NaN(2,length(centers.x));
-    
-    switch model.settings.extraction.interpolationDirection
-        case 'f'
-            %% correct way to average the spectrum
-            borders.x = [1; 1] * centers.x + [-1; 1] .* width/2 * cos(alpha);
-            borders.y = [1; 1] * centers.y + [-1; 1] .* width/2 * sin(alpha);
-        case 'x'
-            %% "wrong" way to average the spectrum
-            % corresponds to the old way of averaging
-            borders.x = [1; 1] * centers.x + [-1; 1] .* width * sin(alpha);
-            borders.y = [1; 1] * centers.y;
-        case 'y'
-            %%
-            borders.x = [1; 1] * centers.x;
-            borders.y = [1; 1] * centers.y + [-1; 1] .* width * cos(alpha);
-        otherwise
-            ex = MException('MATLAB:noSuchAveraging', ...
-                'Not possible to average in direction %s. Chose either x, y or f.', p.Results.averaging);
-            throw(ex)
-    end
-
-    % create positions array for interpolating
-    steps = repmat(transpose(0:(width-1)),1,size(borders.y,2));
-    positions.x = repmat(borders.x(1,:),width,1) + repmat(diff(borders.x,1,1),width,1)./(width-1) .* steps;
-    positions.y = repmat(borders.y(1,:),width,1) + repmat(diff(borders.y,1,1),width,1)./(width-1) .* steps;
-    
-    model.settings.extraction.interpolationPositions = positions;
-    
+function showInterpolationPositions(handles, model)
 %% clean data for plotting to not show values outside the image
-    centers = cleanArray(centers, img);
-    borders = cleanArray(borders, img);
-    positions = cleanArray(positions, img);
+    if isa(model.file, 'Utils.HDF5Storage.h5bm') && isvalid(model.file)
+        img = model.file.readPayloadData(1, 1, 1, 'data');
+    else
+        return;
+    end
+    centers = cleanArray(model.settings.extraction.interpolationCenters, img);
+    borders = cleanArray(model.settings.extraction.interpolationBorders, img);
+    positions = cleanArray(model.settings.extraction.interpolationPositions, img);
     
     function arr = cleanArray(arr, img)
         arr.x(arr.x > size(img,2)) = NaN;
@@ -340,8 +244,13 @@ function getInterpolationPositions(handles, model)
 %% plot
     handles.plotCenters.XData = centers.x;
     handles.plotCenters.YData = centers.y;
-    set(handles.plotBorders, {'XData'}, num2cell(borders.x,2));
-    set(handles.plotBorders, {'YData'}, num2cell(borders.y,2));
+    if ~isempty(borders.x) && ~isempty(borders.y)
+        set(handles.plotBorders, {'XData'}, num2cell(borders.x,2));
+        set(handles.plotBorders, {'YData'}, num2cell(borders.y,2));
+    else
+        set(handles.plotBorders, {'XData'}, {[];[]});
+        set(handles.plotBorders, {'YData'}, {[];[]});
+    end
     delete(model.handles.plotPositions);
     model.handles.plotPositions = plot(handles.axesImage, positions.x, positions.y, 'color', 'yellow');
 end
