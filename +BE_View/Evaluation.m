ad7760547c33fe1dc0166b717a270bd2284a7d38
Conrad Moeckel <conrad.moeckel@biotec.tu-dresden.de>
2017-03-30 12:38:42 +0200

    Do not check validity for brightfield image

--- +BE_View/Evaluation.m
+++ +BE_View/Evaluation.m
@@ -209,12 +209,13 @@ function plotData (handles, model, location, full)
     
     data = model.results.(model.displaySettings.evaluation.type);
     data = double(data);
-    if model.displaySettings.evaluation.discardInvalid && ~strcmp(model.displaySettings.evaluation.type, 'validty')
-        data(~model.results.validity) = NaN;
-        validity = model.results.peaksBrillouin_dev./model.results.peaksBrillouin_int;
-        data(validity > model.displaySettings.evaluation.valThreshould) = NaN;
+    if ~strcmp(model.displaySettings.evaluation.type, 'brightfield')
+        if model.displaySettings.evaluation.discardInvalid && ~strcmp(model.displaySettings.evaluation.type, 'validty')
+            data(~model.results.validity) = NaN;
+            validity = model.results.peaksBrillouin_dev./model.results.peaksBrillouin_int;
+            data(validity > model.displaySettings.evaluation.valThreshould) = NaN;
+        end
     end
-    
     data = nanmean(data,4);
 
     %% find non-singleton dimensions
