288bc1378e8f170fc75d8c01f4e89ee60e31716a
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2017-09-08 15:02:44 +0200

    Adjust auto evaluation template file

--- BrillouinEvaluation_Auto.m
+++ BrillouinEvaluation_Auto.m
@@ -19,7 +19,7 @@ parameters = struct( ...
         'weighted', false ...           % use weighting for calibration calculation?
     ), ...
     'evaluation', struct( ...           % parameters for the evaluation
-        'interpRayleigh', false ...      % interpolate Rayleigh peaks position?
+        'interpRayleigh', true ...      % interpolate Rayleigh peaks position?
     )...
 );
 
@@ -39,14 +39,13 @@ controllers.extraction.setActive();
 controllers.extraction.findPeaks();                 % find the Rayleigh and Brillouin peaks
 
 controllers.calibration.setActive();
-controllers.calibration.findPeaks();          % find the peaks of the current calibration measurement
 controllers.calibration.calibrateAll();       % calibrate the frequency axis using all reference measurements
 
 controllers.peakSelection.setActive();
 % controllers.peakSelection.selectFrequencyRangeRayleigh([275 324], 'pix');       % select the frequency range which should be evaluated
 % controllers.peakSelection.selectFrequencyRangeBrillouin([219 245], 'pix');      % select the frequency range which should be evaluated
-% controllers.peakSelection.selectFrequencyRangeRayleigh([13.1 17.6], 'GHz');     % select the frequency range which should be evaluated
-% controllers.peakSelection.selectFrequencyRangeBrillouin([8.75 10.75], 'GHz');   % select the frequency range which should be evaluated
+controllers.peakSelection.selectFrequencyRangeRayleigh([13.1 17.6], 'GHz');     % select the frequency range which should be evaluated
+controllers.peakSelection.selectFrequencyRangeBrillouin([8.75 10.75], 'GHz');   % select the frequency range which should be evaluated
 
 % start the evaluation process
 controllers.evaluation.setActive();
