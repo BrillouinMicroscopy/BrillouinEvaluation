50570afd1e580937542c79d73f9e283432ea03ea
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-05-15 17:16:50 +0200

    Remove legacy files

--- #scripting/getIntensity1D.m
+++
@@ -1,8 +0,0 @@
-function [intensity] = getIntensity1D(img, positions)
-    
-    [X, Y] = meshgrid(1:size(img,2),1:size(img,1));
-    intensity = interp2(X,Y,img,positions.x,positions.y);
-    
-    intensity = nanmean(intensity, 1);
-    
-end
